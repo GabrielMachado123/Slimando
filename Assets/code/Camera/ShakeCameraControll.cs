6c2a91695312da00a4c8965f10d8e76d64844d08
Hoomanbeans <66372206+Hoomanbeans@users.noreply.github.com>
2022-10-15 02:31:41 +0100

    Added camare shake
    you can now call an instace of the main function that makes the camera shake by just typing ShakeCameraControll.instance.StartShake(length , power , rotation multiplier) in any other script without there being a reference.

---
+++ Assets/code/Camera/ShakeCameraControll.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCameraControll : MonoBehaviour
+{
+    public static ShakeCameraControll instance;
+
+    [SerializeField]
+    private float length, power, rotationMultiplier;
+    private float StartTimeRemaining, ShakePower, ShakeFadeTimer, ShakeRotation;
+
+    void LateUpdate()
+    {
+
+        if (StartTimeRemaining > 0)
+        {
+            StartTimeRemaining -= Time.deltaTime;
+
+            float xAmount = Random.Range(-1f, 1f) * ShakePower;
+            float yAmount = Random.Range(-1f, 1f) * ShakePower;
+
+            transform.position += new Vector3(xAmount, yAmount, 0f);
+
+            ShakePower = Mathf.MoveTowards(ShakePower, 0f, ShakeFadeTimer * Time.deltaTime);
+
+            ShakeRotation = Mathf.MoveTowards(ShakeRotation, 0f, ShakeFadeTimer * rotationMultiplier * Time.deltaTime);
+
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, ShakeRotation * Random.Range(-1f, 1f));
+
+    }
+
+    public void StartShake(float length, float power , float rotateMult)
+    {
+        rotationMultiplier = rotateMult;
+        StartTimeRemaining = length;
+        ShakePower = power;
+
+        ShakeFadeTimer = power / length;
+
+        ShakeRotation = power * rotationMultiplier;
+    }
+
+}
