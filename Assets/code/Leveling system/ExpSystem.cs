d6596fae5991fff3def6aa7c259eed21eefb701b
Genexuz <85886630+Genexuz@users.noreply.github.com>
2022-10-15 02:08:23 +0100

    Level and EXP UI

--- Assets/code/Leveling system/ExpSystem.cs
+++ Assets/code/Leveling system/ExpSystem.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ExpSystem : MonoBehaviour
 {
-    public TextMeshProUGUI currentExpText, expGoalText, currentPlayerLevelText;
+    public TextMeshProUGUI currentPlayerLevelText, expJointText;
     public int currentExp, goalExp, playerLevel;
+    public Slider expSlider;
 
     public static ExpSystem instance;
 
@@ -20,9 +22,11 @@ public class ExpSystem : MonoBehaviour
 
     private void Start()
     {
-        currentExpText.text = "EXP: " + currentExp.ToString();
-        expGoalText.text = "EXP GOAL: " + goalExp.ToString();
         currentPlayerLevelText.text = "Level: " + playerLevel.ToString();
+        expJointText.text = currentExp.ToString() + " / " + goalExp.ToString();
+
+        expSlider.value = currentExp;
+        expSlider.maxValue = goalExp;
 
     }
 
@@ -37,9 +41,11 @@ public class ExpSystem : MonoBehaviour
             goalExp += goalExp / 20;
 
             currentPlayerLevelText.text = "Level: " + playerLevel.ToString();
-            expGoalText.text = "EXP GOAL: " + goalExp.ToString();
+
+            expSlider.maxValue = goalExp;
         }
 
-        currentExpText.text = "EXP: " + currentExp.ToString();
+        expSlider.value = currentExp;
+        expJointText.text = currentExp.ToString() + " / " + expSlider.maxValue;
     }
 }
